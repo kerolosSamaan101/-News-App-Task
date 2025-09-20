1d1026ebd7a755e33eb20c60ac6188715d51922b
kerolos <kerolossamaan34@gmail.com>
2025-09-20 19:52:38 +0300

    Make Detailed Page Scrollable

--- newsapp/lib/Screens/DetailedPage.dart
+++ newsapp/lib/Screens/DetailedPage.dart
@@ -26,43 +26,49 @@ class _DetailedpageState extends State<Detailedpage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // News Image
-            widget.article.imageUrl.isNotEmpty
-                ? Image.network(
-                    widget.article.imageUrl,
-                  )
-                : const Icon(Icons.hide_image_outlined),
-            // News Title
-            Text(
-              widget.article.title,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            // News description
-            Text(
-              widget.article.description,
-              style: TextStyle(),
-            ),
-            Spacer(),
-            // Button to open the full article in the browser
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-              onPressed: () async {
-                final Uri url = Uri.parse(widget.article.url);
-                await launchUrl(
-                  Uri.parse(widget.article.url),
-                  mode: LaunchMode.platformDefault,
-                );
-              },
-              child: const Text(
-                "Go to Article",
-                style: TextStyle(color: Colors.white),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // News Image
+              SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: widget.article.imageUrl.isNotEmpty
+                    ? Image.network(widget.article.imageUrl)
+                    : const Icon(Icons.hide_image_outlined),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 15,
+              ),
+
+              // News Title
+              Text(
+                widget.article.title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+
+              // News description
+              Text(widget.article.description),
+
+              SizedBox(
+                height: 25,
+              ),
+              // Button to open the full article in the browser
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () async {
+                  final Uri url = Uri.parse(widget.article.url);
+                  await launchUrl(url, mode: LaunchMode.platformDefault);
+                },
+                child: const Text(
+                  "Go to Article",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
